419256aa16da21d7844856595e05cf921d29656e
anurag0510 <anurag0510@outlook.com>
2022-12-16 20:25:55 +0530

    env variables defined for usage

---
+++ source/config/vars.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+require("dotenv-safe").config({
+  path: path.join(__dirname, "../../.env"),
+  sample: path.join(__dirname, "../../.env.example"),
+});
+
+module.exports = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  mongo: {
+    uri: process.env.MONGO_URI,
+  },
+  roles: process.env.ROLES,
+};

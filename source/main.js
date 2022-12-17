df5f0e7d5a25cbc18a01e9cd6e40649b54399bb0
anurag0510 <anurag0510@outlook.com>
2022-12-17 20:11:44 +0530

    removed console logging and moved to standard one

--- source/main.js
+++ source/main.js
@@ -2,9 +2,10 @@ const { port, env } = require("./config/vars");
 
 const app = require("./config/express");
 const mongoose = require("./config/mongoConnection");
+const log = require("../source/config/logger");
 
 mongoose.connect();
 
-app.listen(port, () => console.info(`server started on port ${port} (${env})`));
+app.listen(port, () => log.info(`server started on port ${port} (${env})`));
 
 module.exports = app;
